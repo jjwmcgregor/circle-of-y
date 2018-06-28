0097c9b6366c4bb113c5e1e66dfb5fe92dd2f3e6
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-28 14:57:14 +0100

    added password validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,18 @@ class User < ApplicationRecord
     chapter_email;
   end
 
-  validates :password, length: { maximum: 15 }
+
+  PASSWORD_FORMAT = /\A
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+    (?=.*[[:^alnum:]])
+  /x
+
+  validates :password,
+  presence: true,
+  format: { with: PASSWORD_FORMAT, message: " must include at least one lower case letter, one upper case letter, one digit and one special character." }
+
+
 
 end
