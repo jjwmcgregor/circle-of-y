f84942337911c1f4fd6c323614003f0e4a47cf19
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-29 11:09:43 +0100

    if the user does not select a file for a profile picture, a placholder image will be used

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,15 @@ class User < ApplicationRecord
     chapter_email;
   end
 
+
   has_one_attached :image
+
+  def image_filename
+    self.image.filename.to_s if self.image.attached?
+  end
+
+  def image_attached?
+    self.image.attached?
+  end
 
 end
