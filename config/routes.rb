21309f65cfde60ebc85992404b3dc2757270d9fd
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-06-29 12:26:45 +0100

    Added 'Twitter Handle' text to forms and linked user profile image to My Profile/show profile page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
 
   get "wait" => "wait#show"
 
-  # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
