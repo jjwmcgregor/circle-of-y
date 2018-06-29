c888c746da213abca3828899e2b4d5561cb8bdbd
Stephen Reid <steveyblam@hotmail.co.uk>
2018-06-29 19:13:48 +0100

    uglifier flag

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ProjectTest1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    config.assets.js_compressor = Uglifier.new(harmony: true)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
