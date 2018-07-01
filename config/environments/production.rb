82873c1518265d008244a06dd6ebdab3acdcb5cf
Stephen Reid <steveyblam@hotmail.co.uk>
2018-07-01 14:50:13 +0100

    changed host to confirm_url in env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@coyi.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => ENV['HOST'] } 
+  config.action_mailer.default_url_options = { :host => ENV['CONFIRM_URL'] } 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
