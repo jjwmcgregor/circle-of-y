61a162a76c2bb7ff5a2ec7a6717a81a4a5f123dd
Jack McGregor <jackjwmcgregor@gmail.com>
2018-07-09 20:03:47 +0000

    Merged in CW-17_front-page-changes (pull request #16)
    made all required changes to landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require jquery3
 //= require bootstrap
 //= require map
+//= require parallax
 
 
 // window.addEventListener("scroll", function() {
@@ -27,3 +28,50 @@
 //     $('.navbar').removeClass("transparent-bg");
 //   }
 // },false);
+
+$(document).ready(function() {
+
+  $.fn.parallax = function(options) {
+
+    var windowHeight = $(window).height();
+
+    // Establish default settings
+    var settings = $.extend({
+      speed: 0.15
+    }, options);
+
+    // Iterate over each object in collection
+    return this.each( function() {
+
+      // Save a reference to the element
+      var $this = $(this);
+
+      // Set up Scroll Handler
+      $(document).scroll(function(){
+
+        var scrollTop = $(window).scrollTop();
+        var offset = $this.offset().top;
+        var height = $this.outerHeight();
+
+        // Check if above or below viewport
+        if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+          return;
+        }
+
+        var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+        // Apply the Y Background Position to Set the Parallax Effect
+        $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+      });
+    });
+  };
+
+  $('.city-lights').parallax({
+    speed:	0.15
+  });
+
+  $('.city-bg').parallax({
+    speed:	0.25
+  });
+});
