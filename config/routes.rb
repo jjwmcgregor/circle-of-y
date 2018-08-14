42ee0b3ebfbde4dde2f0aac51d506ab7ae670cf2
Jack <jmcgregor@spartaglobal.com>
2018-08-14 11:32:11 +0100

    lost a lot of work

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
+  resources :chapters
+
   get "wait" => "wait#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
